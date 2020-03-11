4d74cb3cb904debf8af6d3b985bccf55e824d484
reneequinn <reneequinn17@gmail.com>
2020-03-11 21:33:28 +1030

    Added README, custom display font and colours to tailwind config, created responsive navbar

---
+++ README.md
@@ -0,0 +1,3 @@
+# Damn Fine Coffee Co.
+
+An online store for a fictional business that sells coffee beans and brewing accessories. Made for my e-commerce major assignment at TAFE SA.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      display: [
+        '"Zilla Slab"',
+        'Georgia',
+        '"Times New Roman"',
+        'Times',
+        'serif'
+      ]
+    },
+    extend: {
+      colors: {
+        brown: '#40250e',
+        coffee: '#d9ab73'
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
